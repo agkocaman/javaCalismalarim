fa9cf1f0387672b4394954eb4ec376c7846c8ce7
gkocaman <gkocaman@akademedya.com.tr>
2022-06-20 17:43:04 +0300

    indeks hesaplama :)

--- bedebKitleEndeksi/src/main.java
+++ bedebKitleEndeksi/src/main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("ndeme");
+       /* Kullanıcıdan aldığınız boy ve kilo değerlerine göre kullanıcının beden kitle indeksini bulun
+       beden kitle indeksi : Kilo / Boy(m) * boy(m)
+
+        */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lütfen Boyunuzu giriniz .. : ");
+        double boy = scanner.nextDouble();
+        System.out.println("Lütfen kilonunzu giriniz... : ");
+        double kilo = scanner.nextDouble();
+        double hesapla = kilo / (boy * boy);
+        System.out.println("Beden Kitle indeksiniz....: " +hesapla);
+
     }
 }
