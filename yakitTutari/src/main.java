d8ec35f992d49235941e7f412ef21e37be077da9
gkocaman <gkocaman@akademedya.com.tr>
2022-06-20 17:54:18 +0300

    bakmadan :)

--- yakitTutari/src/main.java
+++ yakitTutari/src/main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
+        /* bir aracın kilometrede ne kadar yaktığını ve kaç kilometre yaptığı bilgilerini alın ve sürücünün toplam ne kadr ödemesi
+        gerektiğini hesaplayın
+         */
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Kilometreye kaç kuruş yakmakta...: ");
+        double kurus = scanner.nextDouble();
+        System.out.print("Kaç kilometre yaptınız...: ");
+        int yokKm = scanner.nextInt();
+        double kactl = kurus * yokKm;
+        System.out.println("Arabanız "+ kactl +" Turk Lirası yakacaktır.");
+
 
     }
 }
